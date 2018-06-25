8d4c6f3ef97474190e39c1ab9414ff705982143c
Luís Martinho <lmartinho@gmail.com>
2018-06-25 01:07:23 +0100

    Use 32 bit (4 byte) registers instead
    Was wrong to use the 64 bit register to store those double words (longs). 

--- maximum.asm
+++ maximum.asm
@@ -1,9 +1,9 @@
 ; PURPOSE: This program finds the maximum number of a set of data items.
 ; VARIABLES:
+; rsi - Holds the address of the array, due to problems with the effective address using the data_items label.
 ; rbx - Holds the index of the data item being examined
-; rdi - Largest data item found
-; rcx - Holds the address of the array, due to problems with the effective address using the data_items label. I believe there is a better way to do this.
-; rax - Current data item
+; edi - Largest data item found
+; eax - Current data item
 ;
 ; The following memory locations are used:
 ;
@@ -20,23 +20,22 @@ data_items:
 
     global start
 start:
-    mov rbx, 0              ; move 0 into the index register
-    ;mov rax, [data_items + 4 * rbx] results in "Mach-O 64-bit format does not support 32-bit absolute addresses"
-    mov rcx, data_items     ; store the array address in rcx
-    mov rax, [rcx + 4 * rbx]; fetch the current item
-    mov rdi, rax            ; set the current item as the max
+    mov rbx, 0                  ; move 0 into the index register
+    lea rsi, [rel data_items]   ; store the array address (relative to the instruction pointer) in rsi
+    mov eax, [rsi + rbx * 4]    ; fetch the current item
+    mov edi, eax                ; set the current item as the max
 
 start_loop:
-    cmp rax, 0              ; check to see if we've hit the end
+    cmp eax, 0                  ; check to see if we've hit the end
     je loop_exit
-    inc rbx                 ; load next value
-    mov rax, [rcx + 4 * rbx]
-    cmp rax, rdi            ; compare current max and current value
-    jle start_loop          ; jump to loop beginning if the new one isn't bigger
+    inc rbx                     ; load next value
+    mov eax, [rsi + rbx * 4]
+    cmp eax, edi                ; compare current max and current value
+    jle start_loop              ; jump to loop beginning if the new one isn't bigger
 
-    mov rdi, rax            ; make current the new max
-    jmp start_loop          ; jump to loop beginning
+    mov edi, eax                ; make current the new max
+    jmp start_loop              ; jump to loop beginning
 
 loop_exit:
-    mov rax, 0x2000001      ; exit(max)
+    mov rax, 0x2000001 
     syscall
