b2bffd045c7edbd66e1c03a3aeeff07398c3a469
Luís Martinho <lmartinho@gmail.com>
2018-07-06 23:13:56 +0100

    Add a writable 0 byte memory address before the newline
    Don't understand yet why this is required. Could it be an alignment issue?

--- write-newline.asm
+++ write-newline.asm
@@ -3,11 +3,16 @@
     global write_newline
 
     section .data
+; Don't know why this 0 byte is necessary, to make the open syscall
+; in the read-records.asm work, and that makes me sad.
+writable:
+    db 0
+
 newline:
-    db "\n"
+    db `\n`
 
     section .text
-        ST_FILEDES equ 8
+ST_FILEDES equ 8
 
 write_newline:
     push rbp
